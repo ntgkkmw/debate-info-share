16aa6a3d794ae1826c27514c2e2f3f73cc07c3b2
ntgkkmw <103868824+ntgkkmw@users.noreply.github.com>
2024-09-08 22:10:42 +0900

    Add files via upload

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+      <>
+        <title>ディベート情報共有用サイト</title>
+        <meta name="description" content="ご意見をお待ちしております！" />
+        <meta property="og:title" content="ディベート情報共有用サイト" />
+        <meta property="og:description" content="ご意見をお待ちしております！" />
+
+      
+      </>
+    );
+  }
